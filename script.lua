625dae118c25e7933b2ef140420779a43c65f452
Rayan0001 <119808350+Rayan0001@users.noreply.github.com>
2023-12-21 18:49:10 +0100

    Update script.lua
    iy

--- script.lua
+++ script.lua
@@ -4,6 +4,8 @@ game.StarterGui:SetCore("ChatMakeSystemMessage", {
 Text = msg;
 })
 end 
+
+_G.IY = true
 local rolesandcolors = {
     ["Mafia"] = Color3.fromRGB(255,0,0), 
     ["Vampire"] = Color3.fromRGB(64,64,64),
